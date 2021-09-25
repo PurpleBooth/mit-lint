67f8ff54214b0f550e6317c55190be7aac15ec08
Billie Thompson <billie@billiecodes.com>
2021-09-25 09:24:14 +0200

    feat: Add an async interface for linting

---
+++ src/cmd/async_lint.rs
@@ -0,0 +1,52 @@
+use futures::{future, stream, StreamExt};
+use mit_commit::CommitMessage;
+
+use crate::model::{Lints, Problem};
+
+/// Lint a commit message
+///
+/// # Examples
+///
+/// ```rust
+/// use mit_commit::CommitMessage;
+/// use mit_lint::{async_lint, lint, Lints};
+/// use tokio::runtime::Runtime;
+/// let rt = Runtime::new().unwrap();
+/// let actual = rt.block_on(async {
+///     async_lint(
+///         &CommitMessage::from("An example commit message"),
+///         Lints::available().clone(),
+///     )
+///     .await
+/// });
+/// assert!(!actual.is_empty());
+/// ```
+///
+/// ```rust
+/// use mit_commit::CommitMessage;
+/// use tokio::runtime::Runtime;
+/// use mit_lint::{Problem, Code, Lints, Lint, async_lint};
+///
+/// let message:String = "x".repeat(73).into();
+/// let expected = vec![Problem::new(
+///     "Your subject is longer than 72 characters".into(),
+///     "It's important to keep the subject of the commit less than 72 characters because when you look at the git log, that's where it truncates the message. This means that people won't get the entirety of the information in your commit.\n\nPlease keep the subject line 72 characters or under"
+///         .into(),
+///     Code::SubjectLongerThan72Characters,
+/// )];
+/// let rt = Runtime::new().unwrap();
+/// let actual = rt.block_on(async {
+///     async_lint(&CommitMessage::from(message), Lints::new(vec![Lint::SubjectLongerThan72Characters].into_iter().collect())).await
+/// });
+/// assert_eq!(
+///     actual, expected,
+///     "Expected {:?}, found {:?}",
+///     expected, actual
+/// );
+/// ```
+pub async fn async_lint(commit_message: &CommitMessage, lints: Lints) -> Vec<Problem> {
+    stream::iter(lints.into_iter())
+        .filter_map(|lint| future::ready(lint.lint(commit_message)))
+        .collect::<Vec<Problem>>()
+        .await
+}
--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@
 #[macro_use]
 extern crate lazy_static;
 
-pub use cmd::lint;
+pub use cmd::{async_lint, lint};
 pub use model::{Code, Error, Lint, LintError, Lints, Problem, CONFIG_KEY_PREFIX};
 
 mod checks;
