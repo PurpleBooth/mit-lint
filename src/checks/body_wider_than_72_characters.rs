46821320c12f575710bb3f0f6dfd0eeba33af27e
Billie Thompson <billie@billiecodes.com>
2022-10-16 13:33:24 +0200

    fix(src): Do not underline things after the scissors block

--- src/checks/body_wider_than_72_characters.rs
+++ src/checks/body_wider_than_72_characters.rs
@@ -1,6 +1,6 @@
 use std::{ops::Add, option::Option::None};
 
-use miette::SourceOffset;
+use miette::{ByteOffset, SourceOffset};
 use mit_commit::CommitMessage;
 
 use crate::model::{Code, Problem};
@@ -29,37 +29,50 @@ fn has_problem(commit: &CommitMessage<'_>) -> bool {
 const LIMIT: usize = 72;
 
 pub fn lint(commit: &CommitMessage<'_>) -> Option<Problem> {
-    if has_problem(commit) {
-        let commit_text = String::from(commit.clone());
+    if !has_problem(commit) {
+        return None;
+    }
+    let comment_char = commit.get_comment_char().map(|x| format!("{x} "));
+    let commit_text: String = commit.clone().into();
+    let scissors_start_line = commit_text.lines().count()
+        - commit
+            .get_scissors()
+            .map(|s| String::from(s).lines().count())
+            .unwrap_or_default();
+    let labels = commit_text
+        .clone()
+        .lines()
+        .enumerate()
+        .filter(|(_, line)| {
+            comment_char
+                .as_ref()
+                .map_or(true, |comment_char| !line.starts_with(comment_char))
+        })
+        .filter(|(line_index, _)| *line_index < scissors_start_line)
+        .filter(|(line_index, line)| line_index > &0 && line.len() > LIMIT)
+        .map(|(line_index, line)| label_line_over_limit(commit_text.clone(), line_index, line))
+        .collect();
 
-        Some(Problem::new(
+    Some(Problem::new(
             ERROR.into(),
             HELP_MESSAGE.into(),
             Code::BodyWiderThan72Characters,
             commit,
             Some(
-                commit_text
-                    .clone()
-                    .lines()
-                    .enumerate()
-                    .filter(|(line_index, line)| line_index > &0 && line.len() > LIMIT)
-                    .map(|(line_index, line)| {
-                        (
-                            "Too long".to_string(),
-                            SourceOffset::from_location(
-                                commit_text.clone(),
-                                line_index,
-                                LIMIT.add(1),
-                            )
-                            .offset(),
-                            line.len() - (LIMIT),
-                        )
-                    })
-                    .collect(),
+                labels,
             ),
             Some("https://git-scm.com/book/en/v2/Distributed-Git-Contributing-to-a-Project#_commit_guidelines".to_string()),
         ))
-    } else {
-        None
-    }
+}
+
+fn label_line_over_limit(
+    commit_text: String,
+    line_index: usize,
+    line: &str,
+) -> (String, ByteOffset, usize) {
+    (
+        "Too long".to_string(),
+        SourceOffset::from_location(commit_text, line_index, LIMIT.add(1)).offset(),
+        line.len() - (LIMIT),
+    )
 }
