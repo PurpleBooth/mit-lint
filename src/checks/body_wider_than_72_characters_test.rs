ff667e52c2da04dd38da8ee5fa299af12e96a8b2
Billie Thompson <billie@purplebooth.co.uk>
2025-05-10 22:28:13 +0200

    chore: Remove redundant lint commands and simplify test code

--- src/checks/body_wider_than_72_characters_test.rs
+++ src/checks/body_wider_than_72_characters_test.rs
@@ -154,7 +154,7 @@ fn last_line_included() {
             ERROR.into(),
             HELP_MESSAGE.into(),
             Code::BodyWiderThan72Characters,
-            &message.clone().into(),
+            &message.into(),
             Some(vec![("Too long".to_string(), 81, 1)]),
             Some("https://git-scm.com/book/en/v2/Distributed-Git-Contributing-to-a-Project#_commit_guidelines".to_string()),
         )).as_ref(),
